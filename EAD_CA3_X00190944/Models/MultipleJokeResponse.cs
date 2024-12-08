279ab060c438f26f70b9c01c3d6bdc1d19890828
Ken <keneithatillo@gmail.com>
2024-12-08 17:10:53 +0000

    refactor: organise code structure by separating into different files

---
+++ EAD_CA3_X00190944/Models/MultipleJokeResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace EAD_CA3_X00190944.Models;
+
+public class MultipleJokeResponse
+{
+    public List<JokeResponse> Jokes { get; set; } = new List<JokeResponse>();
+}
---
+++ EAD_CA3_X00190944/Utilities/UtilityMethods.cs
@@ -0,0 +1,31 @@
+﻿using EAD_CA3_X00190944.Enums;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace EAD_CA3_X00190944.Utilities;
+
+public static class UtilityMethods
+{
+    public static async Task TriggerGenerateJoke(
+        KeyboardEventArgs e, Func<Task> generateJokeAsync)
+    {
+        if (e.Key == "Enter")
+        {
+            await Task.Delay(1);  // Ensure the search query binding is updated
+            await generateJokeAsync();
+        }
+    }
+
+    public static string GetLanguageDisplayName(Language language)
+    {
+        return language switch
+        {
+            Language.EN => "English",
+            Language.CS => "Czech",
+            Language.FR => "French",
+            Language.DE => "German",
+            Language.PT => "Portuguese",
+            Language.ES => "Spanish",
+            _ => "Unknown"
+        };
+    }
+}
